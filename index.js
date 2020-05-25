2c5d5f3f52e47d2fa853af57718d637c0e8c7520
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-25 13:23:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ console.log(red, orange, yellow, green, blue, indigo, violet);
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
 const [r, o, y, g, b, i, v] = colors;
-console.log(r, o, y, g , b, v); 
+console.log(r, o, y, g, b, v); 
 
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
 
