f8a17527c6afa406c385502bd7f1f531b640d801
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-25 13:17:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -69,10 +69,10 @@ const k_muppet = {
 
 // 7. Use destructuring to assign all appropriate variables using the keys as the variable names
 
-const {name, color, song, job, partner} = muppet;
-const {k_name, k_color, k_album, k_job, k_partner} = k_muppet;
-const {the_muppet_movie} = k_muppet.k_album;
-const {song_1, song_2, song_3, song_4} = k_muppet.k_album.the_muppet_movie;
+const { name, color, song, job, partner } = muppet;
+const { k_name, k_color, k_album, k_job, k_partner } = k_muppet;
+const { the_muppet_movie } = k_muppet.k_album;
+const { song_1, song_2, song_3, song_4 } = k_muppet.k_album.the_muppet_movie;
 
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
